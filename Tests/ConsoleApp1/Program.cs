45ba55bb190ea225719ef9142678b6b1b36a031c
M95N1K <mysnik999@gmail.com>
2021-07-17 12:48:41 +0600

    Добавлено консольное приложение для тестирования классов

---
+++ Tests/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using ThreeInLine;
+using ThreeInLine.Models;
+
+namespace Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestMatrix();
+
+            Console.ReadKey();
+        }
+
+        static void TestMatrix()
+        {
+            int x = 3;
+            int y = 2;
+            Matrix matrix = new Matrix(x, y);
+
+            matrix[0, 0] = 5;
+            matrix[1, 0] = 4;
+            matrix[2, 0] = 3;
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int k = 0; k < x; k++)
+                {
+                    Console.Write($"{matrix[k, i]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
